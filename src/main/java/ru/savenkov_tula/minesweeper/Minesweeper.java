21e1106e5a59f1ae39404c05c0bd5837516c6b6f
savenkov-tula <savenkov-tula@mail.ru>
2020-01-14 19:18:12 +0300

    add cell images and show them

--- src/main/java/ru/savenkov_tula/minesweeper/Minesweeper.java
+++ src/main/java/ru/savenkov_tula/minesweeper/Minesweeper.java
@@ -1,22 +1,39 @@
 package ru.savenkov_tula.minesweeper;
 
+import ru.savenkov_tula.minesweeper.model.Cell;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Minesweeper extends JFrame {
     private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
         new Minesweeper();
     }
 
     private Minesweeper(){
+        setImages();
+        initPanel();
         initFrame();
     }
 
     private void initPanel(){
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500,500));
+        panel = new JPanel()
+        {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Cell cell: Cell.values())
+                g.drawImage((Image) cell.image,
+                        cell.ordinal() * IMAGE_SIZE,0, this);
+            }
+        };
+        panel.setPreferredSize(new Dimension(
+                COLS * IMAGE_SIZE,ROWS * IMAGE_SIZE));
         add(panel);
     }
 
@@ -27,5 +44,18 @@ public class Minesweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+    }
+
+    private void setImages(){
+        for (Cell cell: Cell.values()) {
+            cell.image = getImage(cell.name().toLowerCase());
+        }
+    }
+
+    private Image getImage(String name){
+        String filename = "/img/" + name + ".png";
+
+        ImageIcon icon = new ImageIcon (getClass().getResource(filename));
+        return icon.getImage();
     }
 }
